21e15300c644460ae026096c170decc0f36376af
jtsimmons108 <jsimmons@psdr3.org>
2017-02-25 15:50:34 -0600

    Initial Commit

---
+++ DirectoryTesting/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DirectoryTesting
+//
+//  Created by Jeremiah Simmons on 2/25/17.
+//  Copyright © 2017 Pattonville School District. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
