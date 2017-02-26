52c61322d4e05e772a6567a43a26eff35edfacc1
jtsimmons108 <jsimmons@psdr3.org>
2017-02-26 08:22:41 -0600

    Directory Parsing -- First Commit

---
+++ DirectoryTesting/FacultyMember.swift
@@ -0,0 +1,114 @@
+//
+//  FacultyMember.swift
+//  DirectoryTesting
+//
+//  Created by Jeremiah Simmons on 2/25/17.
+//  Copyright © 2017 Pattonville School District. All rights reserved.
+//
+
+import Foundation
+
+class FacultyMember: CustomStringConvertible{
+    
+    
+    var fname: String, lname: String, long_desc: String, location: String, email: String, office1: String ,ext1: String, office2: String, ext2: String, office3: String, ext3: String
+    
+    static let PRINCIPAL: Int = 0, ASSOCIATE_PRINCIPAL: Int = 1, ASSISTANT_PRINCIPAL:Int = 2,
+        TEACHER: Int = 3, SECRETARY: Int = 4, SUPPORT_STAFF: Int = 5
+    
+    public var description: String { return fname + " " + lname + ", " + long_desc}
+    
+    var directoryKey: String{
+        get{
+            return getSchoolByLocation(location: self.location)
+        }
+    }
+    
+    var rank: Int{
+        get{
+            if self.long_desc.contains("PRINCIPAL"){
+                if self.long_desc.contains("ASSISTANT"){
+                    return FacultyMember.ASSISTANT_PRINCIPAL
+                }else if self.long_desc.contains("ASSOCIATE"){
+                   return FacultyMember.ASSOCIATE_PRINCIPAL
+                }else{
+                    return FacultyMember.PRINCIPAL
+                }
+            }else if self.long_desc.contains("TEACHER"){
+                return FacultyMember.TEACHER
+            }else if self.long_desc.contains("SECRETARY"){
+                return FacultyMember.SECRETARY
+            }else{
+                return FacultyMember.SUPPORT_STAFF
+            }
+        }
+    }
+    
+    init(fname: String, lname: String ,long_desc: String ,location: String ,email: String,office1: String, ext1: String, office2: String, ext2: String, office3: String, ext3: String){
+        self.fname = fname
+        self.lname = lname
+        self.long_desc = long_desc
+        self.location = location
+        self.email = email
+        self.office1 = office1
+        self.ext1 = ext1
+        self.office2 = office2
+        self.ext2 = ext2
+        self.office3 = office3
+        self.ext3 = ext3
+    }
+    
+
+    
+    convenience init(data: [String]) {
+        self.init(
+            fname: data[0].trimmingCharacters(in: CharacterSet.whitespacesAndNewlines),
+            lname: data[1].trimmingCharacters(in: CharacterSet.whitespacesAndNewlines),
+            long_desc: data[2].trimmingCharacters(in: CharacterSet.whitespacesAndNewlines),
+            location: data[3].trimmingCharacters(in: CharacterSet.whitespacesAndNewlines),
+            email: data[4].trimmingCharacters(in: CharacterSet.whitespacesAndNewlines),
+            office1: data[5].trimmingCharacters(in: CharacterSet.whitespacesAndNewlines),
+            ext1: data[6].trimmingCharacters(in: CharacterSet.whitespacesAndNewlines),
+            office2: data[7].trimmingCharacters(in: CharacterSet.whitespacesAndNewlines),
+            ext2: data[8].trimmingCharacters(in: CharacterSet.whitespacesAndNewlines),
+            office3: data[9].trimmingCharacters(in: CharacterSet.whitespacesAndNewlines),
+            ext3: data[10].trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        )
+    }
+    
+    func getSchoolByLocation(location: String) -> String{
+        var school = ""
+        switch(location){
+        case "BRIDGEWAY ELEMENTARY":
+            school = "BR"
+        case "ROBERT DRUMMOND ELEMENTARY":
+            school = "DR"
+        case "LEARNING CENTER":
+            fallthrough
+        case "DISTRICT WIDE":
+            school = "LC"
+        case "HOLMAN MIDDLE SCHOOL":
+            school = "HO"
+        case "PATTONVILLE HEIGHTS":
+            school = "HT"
+        case "POSITIVE SCHOOL":
+            fallthrough
+        case "PATTONVILLE HIGH SCHOOL":
+            school = "HS"
+        case "PARKWOOD ELEMENTARY":
+            school = "PW"
+        case "ROSE ACRES ELEMENTARY":
+            school = "RA"
+        case "REMINGTON TRADITIONAL":
+            school = "RT"
+        case "WILLOW BROOK ELEMENTARY":
+            school = "WB"
+        default:
+            school = "LC"
+        }
+        return school
+    }
+
+    
+    
+}
--- DirectoryTesting/main.swift
+++ DirectoryTesting/main.swift
@@ -8,5 +8,49 @@
 
 import Foundation
 
-print("Hello, World!")
+var directory = [String: [FacultyMember]]()
+
+let filePath = "/Users/jeremiahsimmons/Desktop/Student_Directory.csv"
+var data:String = ""
+do{
+    data = try String(contentsOfFile:filePath, encoding: String.Encoding.utf8)
+}catch{
+    print(error)
+}
+var directoryInfo: [String] = data.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).components(separatedBy: "\n")
+directoryInfo.removeFirst()
+
+
+for person in directoryInfo{
+    var currPerson = FacultyMember(data: person.components(separatedBy: ","))
+    if(directory.index(forKey: currPerson.directoryKey) == nil){
+        directory[currPerson.directoryKey] = []
+    }
+    directory[currPerson.directoryKey]?.append(currPerson)
+}
+
+func sortFacultyMembers(first: FacultyMember, second: FacultyMember) -> Bool{
+    if first.rank == second.rank{
+        if first.long_desc == second.long_desc{
+            if first.lname == second.lname{
+                return first.fname.compare(second.fname).rawValue < 0
+            }else{
+                return first.lname.compare(second.lname).rawValue < 0
+            }
+        }else{
+            return first.long_desc.compare(second.long_desc).rawValue < 0
+        }
+    }else{
+        return first.rank < second.rank
+    }
+}
+
+for school in directory.keys{
+    directory[school] = directory[school]?.sorted(by: sortFacultyMembers)
+}
+
+for i in 0..<directory["HS"]!.count{
+    let long:String = directory["HS"]![i].long_desc
+    print(long.capitalized(with: NSLocale.current))
+}
 
